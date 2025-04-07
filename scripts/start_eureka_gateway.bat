6438adea827496246dfe1489df11894ac01242b3
luccastk <teruyuki710@gmail.com>
2025-04-07 19:24:21 -0300

    refactor: refatoração no service-file e dockers-compose.

--- scripts/start_eureka_gateway.bat
+++ scripts/start_eureka_gateway.bat
@@ -6,7 +6,7 @@ echo ========================================
 echo.
 
 set EUREKA=..\docker-eureka\docker-compose.eureka-dev.yml
-set GATEWAY=..\docker-gateway\docker-compose.gateway.yml
+set GATEWAY=..\docker-gateway\docker-compose.gateway-dev.yml
 
 docker compose -p kit-start-dev --env-file ../.env -f %GATEWAY% -f %EUREKA% up -d --no-build
 
