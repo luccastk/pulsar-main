9d91c8b9059c405d163545fd31b11384eccbb2e1
luccastk <teruyuki710@gmail.com>
2025-04-08 14:40:31 -0300

    ajustando ponteiros

--- scripts/build.bat
+++ scripts/build.bat
@@ -5,9 +5,19 @@ echo       BUILDANDO AMBIENTE COMPLETO
 echo ========================================
 echo.
 
+set KAFKA_FOLDER=..\docker-kafka\KAFKA
+
+if not exist %KAFKA_FOLDER% (
+	mkdir %KAFKA_FOLDER%\data
+	echo Pasta criada; %KAFKA_FOLDER%.
+)  else (
+	echo Pasta já existe: %KAFKA_FOLDER%.
+)
+
 docker compose ^
 	-p complet-stack ^
 	--env-file ..\.env ^
+	-f ..\docker-infra\docker-compose.infra-dev.yml ^
 	-f ..\docker-eureka\docker-compose.eureka-dev.yml ^
 	-f ..\docker-gateway\docker-compose.gateway-dev.yml ^
 	-f ..\docker-kafka\docker-compose.kafka-dev.yml ^
--- scripts/stop.bat
+++ scripts/stop.bat
@@ -9,10 +9,14 @@ set EUREKA=..\docker-eureka\docker-compose.eureka-dev.yml
 set GATEWAY=..\docker-gateway\docker-compose.gateway-dev.yml
 set KAFKA=..\docker-kafka\docker-compose.kafka-dev.yml
 set SERVICE_FILE=..\docker-services\docker-compose.services-dev.yml
+set INFRA=..\docker-infra\docker-compose.infra-dev.yml
+
+rd /S /Q ..\docker-kafka\KAFKA
 
 REM Derrubando containers e volumes
 docker compose -p complet-stack ^
 	--env-file ..\.env ^
+	-f %INFRA% ^
 	-f %EUREKA% ^
 	-f %GATEWAY% ^
 	-f %KAFKA% ^
